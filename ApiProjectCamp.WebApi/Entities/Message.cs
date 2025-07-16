503e99da4082efc63fbe8c221803a811089e5d0f
Melih DİNÇER <m_dincer41@hotmail.com>
2025-07-16 20:33:53 +0300

    Entitylerin tamamlanması ve paketlerin kurulumu tamamlandı.

---
+++ ApiProjectCamp.WebApi/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiProjectCamp.WebApi.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageNameSurname { get; set; }
+        public string MessageEmail { get; set; }
+        public string MessageSubject { get; set; }
+        public string MessageDetails { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
