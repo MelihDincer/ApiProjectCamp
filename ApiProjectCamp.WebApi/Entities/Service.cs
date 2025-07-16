45272b4b26d8b401f8c12f2c1c2d57e0529b8fd7
Melih DİNÇER <m_dincer41@hotmail.com>
2025-07-16 20:11:03 +0300

    Add project files.

---
+++ ApiProjectCamp.WebApi/Entities/Service.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjectCamp.WebApi.Entities
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceTitle { get; set;}
+        public string ServiceDescription { get; set;}
+        public string ServiceIconUrl { get; set;}
+    }
+}
