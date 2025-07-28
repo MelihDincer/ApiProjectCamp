ec203274a486b23d85671646a090dca32318daf8
Melih DİNÇER <m_dincer41@hotmail.com>
2025-07-28 20:32:20 +0300

    Api Consume başlangıcı.

---
+++ ApiProjectCamp.WebUI/Dtos/ServiceDtos/ResultServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjectCamp.WebUI.Dtos.ServiceDtos
+{
+    public class ResultServiceDto
+    {
+        public int ServiceId { get; set; }
+        public string ServiceTitle { get; set; }
+        public string ServiceDescription { get; set; }
+        public string ServiceIconUrl { get; set; }
+    }
+}
