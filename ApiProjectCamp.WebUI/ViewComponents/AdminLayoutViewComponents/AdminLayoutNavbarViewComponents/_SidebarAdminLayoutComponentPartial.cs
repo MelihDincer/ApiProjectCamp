ec3a83484399aa51c6c0adccf4a0a2e68481ba63
Melih DİNÇER <m_dincer41@hotmail.com>
2025-08-06 21:43:25 +0300

    Sidebar ile ilgili çalışmalar yapıldı ve Renderbody eklendi.

---
+++ ApiProjectCamp.WebUI/ViewComponents/AdminLayoutViewComponents/AdminLayoutNavbarViewComponents/_SidebarAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.ViewComponents.AdminLayoutViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _SidebarAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
