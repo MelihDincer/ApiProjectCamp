0f7d49aa290af71513a88aa3c59f8a941d99b383
Melih DİNÇER <m_dincer41@hotmail.com>
2025-08-06 21:22:44 +0300

    Navbar işlemleri tamamlandı.

---
+++ ApiProjectCamp.WebUI/ViewComponents/AdminLayoutViewComponents/AdminLayoutNavbarViewComponents/_NavbarAccountMenuAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebUI.ViewComponents.AdminLayoutViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarAccountMenuAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
