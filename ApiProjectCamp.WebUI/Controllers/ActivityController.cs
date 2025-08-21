a9bf6f8c422ae277497a9b6857d7b858ae6b8e14
Melih DİNÇER <m_dincer41@hotmail.com>
2025-08-21 21:25:17 +0300

    Activity Admin UI işlemleri tamamlandı.

---
+++ ApiProjectCamp.WebUI/Controllers/ActivityController.cs
@@ -0,0 +1,85 @@
+﻿using ApiProjectCamp.WebUI.Dtos.ActivityDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiProjectCamp.WebUI.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ActivityController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> ActivityList()
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7208/api/Activities");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultActivityDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateActivity()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(CreateActivityDto createActivityDto)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createActivityDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7208/api/Activities", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ActivityList");
+            }
+            return RedirectToAction("CreateActivity");
+        }
+
+        public async Task<IActionResult> DeleteActivity(int id)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7208/api/Activities?id=" + id);
+            return RedirectToAction("ActivityList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateActivity(int id)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7208/api/Activities/GetActivity?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<GetByIdActivityDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateActivity(UpdateActivityDto updateActivityDto)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateActivityDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7208/api/Activities", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ActivityList");
+            }
+            return View();
+        }
+    }
+}
---
+++ ApiProjectCamp.WebUI/Dtos/ActivityDtos/UpdateActivityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjectCamp.WebUI.Dtos.ActivityDtos
+{
+    public class UpdateActivityDto
+    {
+        public int ActivityId { get; set; }
+        public string ActivityTitle { get; set; }
+        public string ActivityDescription { get; set; }
+        public string ActivityImageUrl { get; set; }
+        public bool ActivityStatus { get; set; }
+        public decimal ActivityPrice { get; set; }
+    }
+}
