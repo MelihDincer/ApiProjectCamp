5d9a39a5cdeb8f77fd070cbcdd8caec713b2e3f9
Melih DİNÇER <m_dincer41@hotmail.com>
2025-08-21 20:45:08 +0300

    Tesmonial Admin UI işlemleri tamamlandı.

---
+++ ApiProjectCamp.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,85 @@
+﻿using ApiProjectCamp.WebUI.Dtos.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiProjectCamp.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> TestimonialList()
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7208/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createTestimonialDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7208/api/Testimonials", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("TestimonialList");
+            }
+            return RedirectToAction("CreateTestimonial");
+        }
+
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7208/api/Testimonials?id=" + id);
+            return RedirectToAction("TestimonialList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7208/api/Testimonials/GetTestimonial?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<GetByIdTestimonialDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateTestimonialDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7208/api/Testimonials", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("TestimonialList");
+            }
+            return View();
+        }
+    }
+}
---
+++ ApiProjectCamp.WebUI/Dtos/TestimonialDtos/CreateTestimonialDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjectCamp.WebUI.Dtos.TestimonialDtos
+{
+    public class CreateTestimonialDto
+    {
+        public string TestimonialNameSurname { get; set; }
+        public string TestimonialTitle { get; set; }
+        public string TestimonialComment { get; set; }
+        public string TestimonialImageUrl { get; set; }
+    }
+}
