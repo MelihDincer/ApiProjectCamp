b02cfe23c937e0baffafe6429260d7c655b5eb47
Melih DİNÇER <m_dincer41@hotmail.com>
2025-08-23 15:16:28 +0300

    Admin Paneli iletişim işlemleri yapıldı.

---
+++ ApiProjectCamp.WebUI/Dtos/ContactDtos/ResultContactDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjectCamp.WebUI.Dtos.ContactDtos
+{
+    public class ResultContactDto
+    {
+        public int ContactId { get; set; }
+        public string ContactMapLocation { get; set; }
+        public string ContactAddress { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactOpenHours { get; set; }
+    }
+}
