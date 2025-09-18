10f2811adc3c464ab497c33a3c4a5025a159f863
Melih DİNÇER <m_dincer41@hotmail.com>
2025-09-18 20:27:12 +0300

    Dashboard ana grafik düzenlemeleri yapıldı.

--- ApiProjectCamp.WebUI/Models/RevenueChartViewModel.cs
+++ ApiProjectCamp.WebUI/Models/RevenueChartViewModel.cs
@@ -2,19 +2,24 @@
 {
     public class RevenueChartViewModel
     {
-        public List<string> Labels { get; set; } = new();
-        public List<int> Income { get; set; } = new();
-        public List<int> Expense { get; set; } = new();
+        //public List<string> Labels { get; set; } = new();
+        //public List<int> Income { get; set; } = new();
+        //public List<int> Expense { get; set; } = new();
 
-        // Alt kutucuk verileri
-        public decimal WeeklyEarnings { get; set; }
-        public decimal MonthlyEarnings { get; set; }
-        public decimal YearlyEarnings { get; set; }
+        //// Alt kutucuk verileri
+        //public decimal WeeklyEarnings { get; set; }
+        //public decimal MonthlyEarnings { get; set; }
+        //public decimal YearlyEarnings { get; set; }
 
-        public int TotalCustomers { get; set; }
-        public decimal TotalIncome { get; set; }
-        public int ProjectCompleted { get; set; }
-        public decimal TotalExpense { get; set; }
-        public int NewCustomers { get; set; }
+        //public int TotalCustomers { get; set; }
+        //public decimal TotalIncome { get; set; }
+        //public int ProjectCompleted { get; set; }
+        //public decimal TotalExpense { get; set; }
+        //public int NewCustomers { get; set; }
+
+        // İstersen ileride rezervasyonlardan hesaplanacak ek veriler için
+        public int TotalReservations { get; set; }
+        public int ApprovedReservations { get; set; }
+        public int CanceledReservations { get; set; }
     }
 }
