2576752bd620271f78c9735634675d002e25ffcc
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-15 13:07:55 +0300

    Rezervasyon oluşturma talebi işlemleri JS validasyonlarıyla güncel projede entegrasyonları yapılarak tamamlandı.

--- ApiProjectCamp.WebUI/Controllers/DefaultController.cs
+++ ApiProjectCamp.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjectCamp.WebUI.Dtos.ReservationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace ApiProjectCamp.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDto createReservationDto)
+        {
+            ModelState.Remove("ReservationStatus");
+            createReservationDto.ReservationStatus = "Onay Bekliyor";
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                return Content("Model hatası: " + string.Join(", ", errors));
+            }
+         
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createReservationDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("https://localhost:44338/api/Reservations", content);
+
+            if (response.IsSuccessStatusCode)
+                return Content("OK");
+
+            return Content("Bir hata oluştu!");
+        }
+
     }
 }
--- ApiProjectCamp.WebUI/wwwroot/yummy-red-1.0.0/assets/vendor/php-email-form/validate.js
+++ ApiProjectCamp.WebUI/wwwroot/yummy-red-1.0.0/assets/vendor/php-email-form/validate.js
@@ -1,85 +1,56 @@
-/**
-* PHP Email Form Validation - v3.9
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
 (function () {
-  "use strict";
+    "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+    let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
+    forms.forEach(function (form) {
 
-      let thisForm = this;
+        form.addEventListener('submit', function (event) {
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+            event.preventDefault();
 
-      let formData = new FormData( thisForm );
+            let thisForm = this;
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
+            let action = thisForm.getAttribute('action');
+            if (!action) {
+                displayError(thisForm, "Form action property is missing!");
+                return;
             }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
-    });
-  });
 
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
+            thisForm.querySelector('.loading').classList.add('d-block');
+            thisForm.querySelector('.error-message').classList.remove('d-block');
+            thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
-  }
+            let formData = new FormData(thisForm);
+
+            fetch(action, {
+                method: "POST",
+                body: formData
+            })
+                .then(response => response.text())
+                .then(data => {
+
+                    thisForm.querySelector('.loading').classList.remove('d-block');
+
+                    if (data.trim() === "OK") {
+                        thisForm.querySelector('.sent-message').classList.add('d-block');
+                        thisForm.reset();
+                    } else {
+                        displayError(thisForm, data);
+                    }
+
+                })
+                .catch(error => {
+                    displayError(thisForm, error);
+                });
+
+        });
+    });
+
+    function displayError(form, error) {
+        form.querySelector('.loading').classList.remove('d-block');
+        form.querySelector('.error-message').innerHTML = error;
+        form.querySelector('.error-message').classList.add('d-block');
+    }
 
 })();
