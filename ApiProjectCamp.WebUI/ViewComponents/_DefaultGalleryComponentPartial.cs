8ed155df6163a610c46afaeec01f19064072fba6
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-15 15:34:20 +0300

    Anasayfa galeri slider işlemleri tamamlandı.

---
+++ ApiProjectCamp.WebUI/ViewComponents/_DefaultGalleryComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiProjectCamp.WebUI.Dtos.ImageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjectCamp.WebUI.ViewComponents
+{
+    public class _DefaultGalleryComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultGalleryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:44338/api/Images");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultImageDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
